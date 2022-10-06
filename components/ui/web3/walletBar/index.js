b3601c473c8abfb64a2a02b28ed9fb4fa89da4af
Prosenjit Mondal <prosenjit.coder@gmail.com>
2022-10-06 20:07:30 +0600

    add order modal and order card

--- components/ui/web3/walletBar/index.js
+++ components/ui/web3/walletBar/index.js
@@ -3,7 +3,7 @@ import { useWeb3 } from "@components/providers";
 const WalletBar = ({ address, network }) => {
   const { requireInstall } = useWeb3();
   return (
-    <section className="text-white bg-indigo-600">
+    <section className="text-white bg-indigo-600 rounded-lg">
       <div className="p-8">
         <h1 className="text-2xl">Hello, {address}</h1>
         <h2 className="subtitle mb-5 text-xl">
