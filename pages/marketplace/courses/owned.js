22d430a3a9c68c2f29b5b251b797ae1d9e9f710b
Prosenjit Mondal <prosenjit.coder@gmail.com>
2022-10-11 10:21:08 +0600

    add component changes and message component

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,3 +1,4 @@
+import { Button, Message } from "@components/ui/common";
 import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
@@ -8,8 +9,11 @@ const OwnedCourses = () => {
       <div className="py-4">
         <MarketHeader />
       </div>
-      <section>
-        <OwnedCourseCard />
+      <section className="grid grid-cols-1">
+        <OwnedCourseCard>
+          <Message>My custom message!</Message>
+          <Button>Watch the course</Button>
+        </OwnedCourseCard>
       </section>
     </BaseLayout>
   );
